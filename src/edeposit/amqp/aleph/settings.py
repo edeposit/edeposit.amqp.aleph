880a129bb44c313c528c0bd99325239d2302cde7
Bystroushaak <bystrousak@kitakitsune.org>
2014-02-07 09:27:29 +0100

    PEP8 aplied.

--- src/edeposit/amqp/aleph/settings.py
+++ src/edeposit/amqp/aleph/settings.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 import os.path
 
-BASE_PATH=(os.path.dirname(__file__))
+
+BASE_PATH = (os.path.dirname(__file__))
 
 ALEPH_URL = "http://aleph.nkp.cz"
-PATH_OF_EXPORT_DIRECTORY=os.path.join(BASE_PATH,"tests","var","export")
-
+PATH_OF_EXPORT_DIRECTORY = os.path.join(BASE_PATH, "tests", "var", "export")
