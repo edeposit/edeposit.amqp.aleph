30b03faeffc637842a3a832c061c73f137538aa1
Bystroushaak <bystrousak@kitakitsune.org>
2014-02-09 03:55:25 +0100

    setup.py cleaned a little.

--- setup.py
+++ setup.py
@@ -1,51 +1,63 @@
+from strings import Template
 from setuptools import setup, find_packages
-import os
 
 version = '0.1'
+long_description = """$README
 
-long_description = (
-    open('README.txt').read()
-    + '\n' +
-    'Contributors\n'
-    '============\n'
-    + '\n' +
-    open('CONTRIBUTORS.txt').read()
-    + '\n' +
-    open('CHANGES.txt').read()
-    + '\n')
+Contributors
+============
+$CONTRIBUTORS
 
-setup(name='edeposit.amqp.aleph',
-      version=version,
-      description="E-Deposit AMQP module for communication with Aleph",
-      long_description=long_description,
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
+Changes
+=======
+$CHANGES
+"""
+
+
+setup(
+    name='edeposit.amqp.aleph',
+    version=version,
+    description="E-Deposit AMQP module for communication with Aleph",
+    long_description=Template(long_description).substitute(
+        README=open('README.txt').read(),
+        CONTRIBUTORS=open('CONTRIBUTORS.txt').read(),
+        CHANGES=open('CHANGES.txt').read()
+    ),
+
+    url='https://github.com/jstavel/edeposit.amqp.aleph',
+
+    author='',
+    author_email='',
+
+    classifiers=[
         "Programming Language :: Python",
+    ],
+    keywords='',
+    license='gpl',
+
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+
+    namespace_packages=[
+        'edeposit',
+        'edeposit.amqp'
+    ],
+    include_package_data=True,
+
+    zip_safe=False,
+    install_requires=['setuptools'],
+    extras_require={
+        "test": [
+            "unittest2",
+            "robotsuite",
+            "mock",
         ],
-      keywords='',
-      author='',
-      author_email='',
-      url='https://github.com/jstavel/edeposit.amqp.aleph',
-      license='gpl',
-      packages=find_packages('src'),
-      package_dir = {'': 'src'},
-      namespace_packages=['edeposit', 'edeposit.amqp'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-      ],
-      extras_require={"test": [
-          "unittest2",
-          "robotsuite",
-          "mock",
-      ], "docs": [
-          "sphinxcontrib-robotdoc",
-          "sphinx",
-      ]},
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
+        "docs": [
+            "sphinxcontrib-robotdoc",
+            "sphinx",
+        ]
+    },
+    # entry_points="""
+    # # -*- Entry points: -*-
+    # """,
+)
