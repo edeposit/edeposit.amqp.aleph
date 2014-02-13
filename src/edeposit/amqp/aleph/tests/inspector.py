912269946b26c36646d677fcf175706161461540
Bystroushaak <bystrousak@kitakitsune.org>
2014-02-13 15:29:41 +0100

    PEP8 aplied to tests/inspector.py.

--- src/edeposit/amqp/aleph/tests/inspector.py
+++ src/edeposit/amqp/aleph/tests/inspector.py
@@ -8,10 +8,11 @@ import inspect
 import imp
 import edeposit.amqp.aleph
 import shutil
-# import variables # TODO: az bude fixnute pojmenovani EPublication
+import variables
 import os.path
 
-BASE_PATH=os.path.basename(__file__)
+BASE_PATH = os.path.basename(__file__)
+
 
 class Inspector(object):
     ROBOT_LIBRARY_SCOPE = 'TEST SUITE'
@@ -19,33 +20,35 @@ class Inspector(object):
     def get_module_variables(self):
         pass
 
-    def variable_presented(self,modulePath,name):
+    def variable_presented(self, modulePath, name):
         #import sys, pdb; pdb.Pdb(stdout=sys.__stdout__).set_trace()
-        module = imp.load_source("module",modulePath)
-        value = getattr(module,name)
+        module = imp.load_source("module", modulePath)
+        value = getattr(module, name)
         if not value:
-            raise AssertionError("module: %s has no variable '%s'" % (self.modulePath, name))
-    
+            raise AssertionError(
+                "module: %s has no variable '%s'" % (self.modulePath, name)
+            )
+
     def is_type_of(self, element, reference):
         if type(element) != reference:
             raise AssertionError("wrong type")
 
-    def testedlibrary_send_to_aleph(self,epublication):
+    def testedlibrary_send_to_aleph(self, epublication):
         return edeposit.amqp.aleph.send_to_aleph(epublication)
 
-    def testedlibrary_handle_aleph_response(self,epublication):
+    def testedlibrary_handle_aleph_response(self, epublication):
         return edeposit.amqp.aleph.handle_aleph_response(epublication)
 
-    def testedLibrary_id_conforms_naming_way(self,epublicationDirectory):
+    def testedLibrary_id_conforms_naming_way(self, epublicationDirectory):
         """ id je ve tvaru: 2014-01-01-agafdsX. A je to zaroven adresar. """
-        if not re.search("[0-9]{4}-[0-9]{2}-[0-9]{2}-.+",epublicationDirectory):
+        if not re.search("[0-9]{4}-[0-9]{2}-[0-9]{2}-.+", epublicationDirectory):
             AssertionError("epublication directory doesnot conform naming way")
 
-    def aleph_AcceptedEPublication(self,directory):
+    def aleph_AcceptedEPublication(self, directory):
         """ created responses the same way as Aleph creates it """
         pass
 
-    def aleph_RejectedEPublication(self,directory):
+    def aleph_RejectedEPublication(self, directory):
         """ created responses the same way as Aleph creates it """
         pass
 
@@ -63,8 +66,7 @@ class Inspector(object):
         - aleph-success
         """
         directory = os.path.join(variables.PATH_OF_EXPORT_DIRECTORY, export_id)
-        shutil.copyfile(os.path.join(BASE_PATH, "aleph-record.xml"), 
+        shutil.copyfile(os.path.join(BASE_PATH, "aleph-record.xml"),
                         os.path.join(directory, "aleph-record.xml"))
-        shutil.copyfile(os.path.join(BASE_PATH, "aleph-success"), 
+        shutil.copyfile(os.path.join(BASE_PATH, "aleph-success"),
                         os.path.join(directory, "aleph-success"))
-        pass
