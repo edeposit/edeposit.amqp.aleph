005fc2154f43130dc3f8a7511d2e2b80ecc9cdf7
Bystroushaak <bystrousak@kitakitsune.org>
2014-02-14 15:33:52 +0100

    Added temporary value for INPUT_EXCHANGE_FOR_ALEPH_SEARCH.

--- src/edeposit/amqp/aleph/settings.py
+++ src/edeposit/amqp/aleph/settings.py
@@ -7,6 +7,7 @@ BASE_PATH = (os.path.dirname(__file__))
 ALEPH_URL = "http://aleph.nkp.cz"
 PATH_OF_EXPORT_DIRECTORY = os.path.join(BASE_PATH, "tests", "var", "export")
 
+INPUT_EXCHANGE_FOR_ALEPH_SEARCH = "aleph-search" # TODO: sync with project
 
 # TODO: remove, for test purposes only
 # https://raw.github.com/jstavel/edeposit.amqp/master/edeposit/amqp/settings.py
