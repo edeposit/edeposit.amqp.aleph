7edde57b8894a044094b6f33e273f4948149ebea
Bystroushaak <bystrousak@kitakitsune.org>
2014-02-17 15:35:17 +0100

    Added AMQPMessage definition for test purposes.
    Definition came from https://github.com/jstavel/edeposit.amqp

--- src/edeposit/amqp/__init__.py
+++ src/edeposit/amqp/__init__.py
@@ -1,1 +1,15 @@
-__import__('pkg_resources').declare_namespace(__name__)
+from collections import namedtuple
+
+class AMQPMessage(namedtuple('AMQPMessage',
+                             ['data',
+                              'headers',
+                              'properties'
+                              ])):
+    """
+    data ... serialized main message
+    headers
+    """
+    pass
+
+
+
