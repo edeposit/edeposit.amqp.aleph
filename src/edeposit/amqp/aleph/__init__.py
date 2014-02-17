0cc482013c8c3657753dd35a6f1e9a90b60fe409
Bystroushaak <bystrousak@kitakitsune.org>
2014-02-17 11:38:16 +0100

    Changes to better reflect new philosophical way of AMQP communication.
    Structures redefined to better accomodate Aleph's workflow.
    Added some docstrings.

--- src/edeposit/amqp/aleph/__init__.py
+++ src/edeposit/amqp/aleph/__init__.py
@@ -3,9 +3,12 @@
 # This package may contain traces of nuts
 from collections import namedtuple
 
+
 ###############################################################################
 # Add new record to Aleph #####################################################
 ###############################################################################
+#
+## Aleph's data wrappers ######################################################
 class Author(namedtuple("Author", ['firstName', 'lastName', 'title'])):
     pass
 
@@ -55,14 +58,89 @@ class OriginalFile(namedtuple("OriginalFile",
     pass
 
 
+## Protocol query wrappers ####################################################
+class AlephExport(namedtuple("AlephExport",
+                             ['epublication',
+                              'linkOfEPublication'])):
+    """ epublication ... type of EPublication
+    linkOfEPublication  ... url with epublication
+
+    User will fill this record.
+    """
+    pass
+
+
+class ExportRequest(namedtuple("ExportRequest",
+                               ['export',
+                                'UUID'])):
+    pass
+
+
+class AlephExportResult(namedtuple("AlephExportResult",
+                                   ['docNumber',
+                                    'base',
+                                    'xml',
+                                    'success',
+                                    'message'])):
+    """ docNumber ... docNumber of a record in Aleph
+    base      ... base of Aleph
+    success   ... whether import was successfull
+    message   ... message of error or success
+    """
+    pass
+
+
+class ExportResult(namedtuple("ExportResult",
+                              ['result',
+                               'UUID'])):
+    """
+    ... result is type of AlephExportResult
+    ... UUID is UUID used in ExportRequest
+    """
+    pass
+
+
 ###############################################################################
 # Search Aleph ################################################################
 ###############################################################################
-class AlephQuery(namedtuple("AlephQuery",
-                            ['base',
-                             'phrase',
-                             'considerSimilar',
-                             'field'])):
+"""
+Workflow is pretty simple:
+
+To query Aleph, just create one of the Queries - ISBNQuery for example and put
+it into SearchRequest wrapper with UUID. Then encode it by calling 
+toAMQPMessage() and send the message to the Aleph exchange.
+
+---
+isbnq = ISBNQuery("0-251-0225-4")
+request = SearchRequest(isbnq, UUID)
+
+amqp.send(
+    toAMQPMessage(request)
+    "ALEPH'S_EXCHANGE"
+)
+---
+
+and you will get back AMQP message, and after decoding with fromAMQPMessage()
+also SearchResult.
+
+If you want to just get count of how many items is there in Aleph (you should
+use this instead of just calling len() to SearchResult.records - it doesn't put
+that much load to Aleph), just wrap the ISBNQuery with CountQuery:
+
+---
+isbnq = CountQuery(ISBNQuery("0-251-0225-4"))
+# rest is same..
+---
+
+and you will get back (after decoding) CountResult.
+"""
+
+
+class GenericQuery(namedtuple("GenericQuery",
+                              ['base',
+                               'phrase',
+                               'considerSimilar',
+                               'field'])):
     """
     base ... base in Aleph
     NKC, ...
@@ -71,96 +149,59 @@ class AlephQuery(namedtuple("AlephQuery",
     pass
 
 
-class SearchRequest(namedtuple("QueryRequest",
-                              ['query',
-                               'UUID'
-                           ])):
+class ISBNQuery(namedtuple("ISBNQuery", ["ISBN"])):
+    pass
+
+
+class AuthorQuery(namedtuple("AuthorQuery", ["author"])):
+    pass
+
+
+class PublisherQuery(namedtuple("PublisherQuery", ["publisher"])):
+    pass
+
+
+class CountQuery(namedtuple("CountQuery", ["type"])):
     """
-    UUID is used as identification of a query in a result response
-    query is type of AlephQuery
+    Put one of the Queries to .type properties and it will return just number
+    of records, instead of records itself.
     """
+    pass
+
+
+class SearchRequest(namedtuple("SearchRequest",
+                               ['query',
+                                'UUID'])):
+    """
+    query -- GenericQuery, ISBNQuery, .. *Query
+    UUID -- identification of a query in a result response
+    """
+
 
 class AlephRecord(namedtuple("AlephRecord",
                              ['base',
                               'docNumber',
                               'xml',
-                              'epub'])):
+                              'epublication'])):
     pass
+
 
 class SearchResult(namedtuple("SearchResult",
                               ['records',
-                               'UUID',
-                           ])):
+                               'UUID'])):
     pass
     """ result of search request """
 
 
-
-###############################################################################
-# Count Aleph #################################################################
-###############################################################################
-
-class CountRequest(AlephQueryRequest):
-    """
-    The same structure as aleph query request.
-    It is used for count requests. Application wants to know number of records only.
-    """
-    pass
-
-
-
-class CountResult(namedtuple("AlephCountResult",
+class CountResult(namedtuple("CountResult",
                              ['num_of_records',
-                              'UUID',
-                          ])):
-    pass
-
-###############################################################################
-# Export to Aleph #############################################################
-###############################################################################
-
-class AlephExport(namedtuple("AlephExport",
-                             ['epublication',
-                              'linkOfEPublication'])):
-    """ epublication ... type of EPublication
-    linkOfEPublication  ... url with epublication
-    
-    User will fill this record.
-    """
-    pass
-
-class ExportRequest(namedtuple("ExportRequest",
-                               ['export',
-                                'UUID'])):
-    pass
-
-class AlephExportResult(namedtuple("AlephExportResult",
-                              ['docNumber',
-                               'base',
-                               'xml',
-                               'success',
-                               'message'])):
-    """ docNumber ... docNumber of a record in Aleph
-    base      ... base of Aleph
-    success   ... whether import was successfull
-    message   ... message of error or success
-    """
-    pass
-
-class ExportResult(namedtuple("ExportResult",
-                              ['result',
-                               'UUID'])):
-    """ 
-    ... result is type of AlephExportResult
-    ... UUID is UUID used in ExportRequest
-    """
+                              'UUID'])):
     pass
 
 
 ###############################################################################
 #  Interface for an external world  ###########################################
 ###############################################################################
-
 def toAMQPMessage(request):
     """ returns  edeposit.amqp.AMQPMessage """
     pass
