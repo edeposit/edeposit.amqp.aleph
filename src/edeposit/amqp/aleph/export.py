a11a823590aa2a22134cf437940571d80ba9079b
Bystroushaak <bystrousak@kitakitsune.org>
2014-03-06 13:28:52 +0100

    Fixed few bugs, export should be now working.

--- src/edeposit/amqp/aleph/export.py
+++ src/edeposit/amqp/aleph/export.py
@@ -6,9 +6,9 @@
 #= Imports ====================================================================
 import isbn
 import settings
-from __init__ import EPublication
+# from __init__ import EPublication, FormatEnum, Author
 
-from httpkie import Downloader()
+from httpkie import Downloader
 
 
 #= Variables ==================================================================
@@ -23,7 +23,7 @@ class ExportException(Exception):
 
 class InvalidISBNException(ExportException):
     def __init__(self, message):
-        super(InvalidAlephBaseException, self).__init__(message)
+        super(InvalidISBNException, self).__init__(message)
 
 
 class ExportData:
@@ -34,7 +34,7 @@ class ExportData:
             "P0200FMT__": "BK",
             "P0300BAS__a": "30",
             "P0501010__a": "",    # ISBN (uppercase)
-            "P0502010__b": ["brož.", "váz.", "mapa", "CD-ROM", "DVD", "online"],  # vazba/forma
+            "P0502010__b": "",    # vazba/forma
             "P0504010__d": "",    # cena
             # "P1201901__b": "",    # ean
             "P0601010__a": "",    # ISBN souboru
@@ -112,17 +112,17 @@ class ExportData:
         )
 
     def _apply_mapping(self, mapping):
-        self.__POST["P0100LDR__"] = maping[0]
-        self.__POST["P0200FMT__"] = maping[1]
-        self.__POST["P0502010__b"] = maping[2]
-        self.__POST["P07022001_b"] = maping[3]
-        self.__POST["P1501IST1_a"] = maping[4]
+        self.__POST["P0100LDR__"] = self.maping[0]
+        self.__POST["P0200FMT__"] = self.maping[1]
+        self.__POST["P0502010__b"] = self.maping[2]
+        self.__POST["P07022001_b"] = self.maping[3]
+        self.__POST["P1501IST1_a"] = self.maping[4]
 
     def _import_epublication(self, epub):
         self.__POST["P0501010__a"] = epub.ISBN
         self.__POST["P07012001_a"] = epub.nazev
         self.__POST["P07032001_e"] = epub.podnazev
-        self.__POST["P0502010__b"] = epub.vazba  # TODO: přidat nějaké ověřování
+        self.__POST["P0502010__b"] = epub.vazba
         self.__POST["P0504010__d"] = epub.cena
         self.__POST["P07042001_h"] = epub.castDil
         self.__POST["P07052001_i"] = epub.nazevCasti
@@ -130,23 +130,22 @@ class ExportData:
         self.__POST["P0903210__d"] = epub.datumVydani
         self.__POST["P0801205__a"] = epub.poradiVydani
         self.__POST["P1502IST1_b"] = epub.zpracovatelZaznamu
-        # self.__POST[""] = epub.kategorieProRIV  # TODO: wtf?
-        self.__POST[""] = epub.datumProCopyright  # wut
         self.__POST["P0503010__x"] = epub.format
         self.__POST["P110185640u"] = epub.url
         self.__POST["P0901210__a"] = epub.mistoVydani
         self.__POST["P0601010__a"] = epub.ISBNSouboruPublikaci
 
+        # FUTURE:
         # self.__POST[""] = epub.mistoDistribuce
         # self.__POST[""] = epub.distributor
         # self.__POST[""] = epub.datumDistribuce
+        # self.__POST[""] = epub.kategorieProRIV  # TODO: wtf?
+        # self.__POST[""] = epub.datumProCopyright  # wut
 
         authors = [x.lastName + ", " + x.firstName for x in epub.autori]
         authors_fields = ["P1301ZAK__b", "P1302ZAK__c", "P1303ZAK__c"]
-        map(
-            lambda field, author: self.__POST[field]=author,
-            zip(authors_fields, authors)
-        )
+        for field, author in zip(authors_fields, authors):
+            self.__POST[field] = author
 
     def get_POST_data(self):
         self._postprocess()
@@ -155,7 +154,7 @@ class ExportData:
 
 
 def exportEpublication(epub):
-    addr = "http://aleph.nkp.cz/aleph-cgi/isxn"
+    # ALEPH_EXPORT_URL
 
     POST = ExportData(epub).get_POST_data()
 
@@ -165,4 +164,3 @@ def exportEpublication(epub):
 #= Main program ===============================================================
 if __name__ == '__main__':
     pass
-
