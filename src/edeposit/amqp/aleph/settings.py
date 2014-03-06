b58261547557c7a66ab5d97ae1415c9529d917f8
Bystroushaak <bystrousak@kitakitsune.org>
2014-03-06 09:29:49 +0100

    Added two new configuration variables.
    + EDEPOSIT_EXPORT_SIGNATURE
    + ALEPH_EXPORT_URL

--- src/edeposit/amqp/aleph/settings.py
+++ src/edeposit/amqp/aleph/settings.py
@@ -4,3 +4,6 @@ import os.path
 
 BASE_PATH = (os.path.dirname(__file__))
 ALEPH_URL = "http://aleph.nkp.cz"
+
+EDEPOSIT_EXPORT_SIGNATURE = "edeposit"
+ALEPH_EXPORT_URL = "http://aleph.nkp.cz/aleph-cgi/isxn"
