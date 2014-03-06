00d7adb475127a902a5339b684fa0227a735c39f
Bystroushaak <bystrousak@kitakitsune.org>
2014-03-06 13:23:30 +0100

    EPublication changed to reflect Aleph's export form. Added documentation.

--- src/edeposit/amqp/aleph/__init__.py
+++ src/edeposit/amqp/aleph/__init__.py
@@ -226,7 +226,7 @@ class ISBNQuery(namedtuple("ISBNQuery", ["ISBN"]), _QueryTemplate):
     Query for ISBN.
 
     Note: ISBN is not unique, so you can get back lot of books with same ISBN.
-          some books also have two or more ISBNs.
+          Some books also have two or more ISBNs.
     """
     def _getIDs(self):
         return aleph.getISBNsIDs(self.ISBN)
@@ -260,14 +260,23 @@ class Author(namedtuple("Author", ['firstName', 'lastName', 'title'])):
     pass
 
 
-class Producent(namedtuple("Producent", ['title',
-                                         'phone',
-                                         'fax',
-                                         'email',
-                                         'url',
-                                         'identificator',
-                                         'ico'])):
-    pass
+# class Producent(namedtuple("Producent", ['title',
+#                                          'phone',
+#                                          'fax',
+#                                          'email',
+#                                          'url',
+#                                          'identificator',
+#                                          'ico'])):
+#     pass
+
+
+class FormatEnum:
+    BROZ = "brož."
+    VAZANA = "váz."
+    MAPA = "mapa"
+    CD = "CD-ROM"
+    DVD = "DVD"
+    ONLINE = "online"
 
 
 class EPublication(namedtuple("EPublication", ["ISBN",
@@ -281,17 +290,17 @@ class EPublication(namedtuple("EPublication", ["ISBN",
                                                'datumVydani',
                                                'poradiVydani',
                                                'zpracovatelZaznamu',
-                                               'kategorieProRIV',
-                                               'mistoDistribuce',
-                                               'distributor',
-                                               'datumDistribuce',
-                                               'datumProCopyright',
+                                               # 'mistoDistribuce',  # FUTURE
+                                               # 'distributor',
+                                               # 'datumDistribuce',
+                                               # 'datumProCopyright',
                                                'format',
                                                'url',
                                                'mistoVydani',
                                                'ISBNSouboruPublikaci',
                                                'autori',
-                                               'originaly'])):
+                                               'originaly'
+                                               'internal_url'])):
     """
     This structure is returned as result of users SearchRequest. It will be
     also used in exporting new data to aleph, but that is not implemented yet.
@@ -299,25 +308,42 @@ class EPublication(namedtuple("EPublication", ["ISBN",
     In case of Search/Count requests, this structure is filled with data from
     MARC XML record parsed by marcxml.py.
 
-    .autori -- list of Author objects
+    Properties:
+
+        .url    -- url specified by publisher (THIS IS NOT INTERNAL URL!)
+        .ISBN   -- ISBN of the book
+        .cena   -- price of the book
+        .nazev  -- name of the book
+        .format -- format of the book - see FormatEnum
+        .autori     -- list of Author objects
+        .castDil    -- which part of the series of books is this
+        .podnazev   -- subname of the book
+        .originaly  -- list of ISBN's of original books in case of
+                       translations
+        .nazevCasti -- name of part of the series
+        .datumVydani  -- date of publication
+        .mistoVydani  -- city/country origin of the publication
+        .internal_url -- link to edeposit/kramerius system
+        .poradiVydani -- order of publication
+        .zpracovatelZaznamu   -- processor/manufacturer of record
+        .nakladatelVydavatel  -- publisher's name
+        .ISBNSouboruPublikaci -- ISBN of the book series
     """
     pass
 
 
-class OriginalFile(namedtuple("OriginalFile", ['url',
-                                               'format',
-                                               'file',
-                                               'isbns'])):
-    """ type of isbn: ISBN"""
-    pass
+# class OriginalFile(namedtuple("OriginalFile", ['url',
+#                                                'format',
+#                                                'file',
+#                                                'isbns'])):
+#     """ type of isbn: ISBN"""
+#     pass
 
 
 ## Export protocol query wrappers #############################################
-class ExportRequest(namedtuple("AlephExport", ['epublication',
-                                               'linkOfEPublication'])):
+class ExportRequest(namedtuple("AlephExport", ['epublication'])):
     """
-    epublication -- type of EPublication
-    linkOfEPublication -- url of epublication
+    epublication -- EPublication structure, which will be exported to Aleph
     """
     pass
 
