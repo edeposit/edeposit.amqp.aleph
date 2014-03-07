c58fd0ec7c43b266fab0eeaeb8d0551be2a5ccf3
Bystroushaak <bystrousak@kitakitsune.org>
2014-03-07 16:11:09 +0100

    Added documentation to export submodule.

--- src/edeposit/amqp/aleph/export.py
+++ src/edeposit/amqp/aleph/export.py
@@ -3,16 +3,33 @@
 #
 # Interpreter version: python 2.7
 #
+"""
+This module is used to put data to Aleph.
+
+It is based on custom made webform, which is currently used to report new
+books by publishers.
+
+Source code of this form is not available at this moment (it was created by
+third party), but it is possible, that it will be in future. This will highly
+depend on number of people, which will use this project.
+
+Most important function from this module is exportEPublication(epub), which
+will do everything, that is needed to do, to export EPublication structure to
+the Aleph.
+
+This whole module is highly dependent on processes, which are defined as import
+processes at the Czech National Library.
+
+If you want to use export ability in your library, you should rewrite this and
+take care, that you are sending data somewhere, where someone will process
+them. Otherwise, you can fill your library's database with crap.
+"""
 #= Imports ====================================================================
 import isbn
 import settings
 from datastructures import FormatEnum
 
 from httpkie import Downloader
-
-
-#= Variables ==================================================================
-
 
 
 #= Functions & objects ========================================================
@@ -32,6 +49,16 @@ class ExportRejectedException(ExportException):
 
 
 class PostData:
+    """
+    This class is used to transform data from EPublication to dictionary, which
+    is sent as POST request to Aleph third-party webform.
+
+    http://aleph.nkp.cz/F/?func=file&file_name=service-isbn
+
+    Class is used, because there is 29 POST parameters with internal
+    dependencies, which need to be processed and validated before they can be
+    passed to webform.
+    """
     def __init__(self, epub):
         self._POST = {
             "sid": settings.EDEPOSIT_EXPORT_SIGNATURE,
@@ -102,6 +129,9 @@ class PostData:
         self._import_epublication(epub)
 
     def _import_epublication(self, epub):
+        """
+        Fill internal property ._POST dictionary with data from EPublication.
+        """
         self._POST["P0501010__a"] = epub.ISBN
         self._POST["P07012001_a"] = epub.nazev
         self._POST["P07032001_e"] = epub.podnazev
@@ -131,6 +161,9 @@ class PostData:
             self._POST[field] = author
 
     def _apply_mapping(self, mapping):
+        """
+        Map some case specific data to the fields in internal dictionary.
+        """
         self._POST["P0100LDR__"] = mapping[0]
         self._POST["P0200FMT__"] = mapping[1]
         self._POST["P0502010__b"] = mapping[2]
@@ -138,6 +171,10 @@ class PostData:
         self._POST["P1501IST1_a"] = mapping[4]
 
     def _postprocess(self):
+        """
+        Move data between internal fields, validate them and make sure, that
+        everything is as it should be.
+        """
         # get ISBN of serie
         series_isbn = self._POST["P0601010__a"]
         if isinstance(series_isbn, list) and len(series_isbn) > 0:
@@ -155,6 +192,7 @@ class PostData:
 
             self._POST["P0601010__b"] = "soubor : " + series_isbn
 
+        # validate ISBN of the book
         book_isbn = self._POST["P0501010__a"]
         if isinstance(book_isbn, list) and len(book_isbn) > 0:
             book_isbn = book_isbn[0]
@@ -171,7 +209,16 @@ class PostData:
             self.mapping.get(self._POST["P0502010__b"], self.mapping["else"])
         )
 
+        # # clean the dict before you send it
+        # for key in self._POST.keys():
+        #     if self._POST[key] == "":
+        #         del self._POST[key]
+
     def _check_required_fields(self):
+        """
+        Make sure, that internal dictionary contains all fields, which are
+        requiered by the webform.
+        """
         assert(self._POST["P0501010__a"] != "")  # ISBN
         assert(self._POST["P1601ISB__a"] != "")  # hidden ISBN field
 
@@ -202,6 +249,13 @@ class PostData:
 
 
 def _sendPostDict(post_dict):
+    """
+    Send `post_dict` to the settigns.ALEPH_EXPORT_URL.
+
+    It may look strange, to have standalone function to do this, but it is used
+    in unittests to test that form rejects invalid data, which can tell us,
+    when the webform is broken.
+    """
     downer = Downloader()
     data = downer.download(settings.ALEPH_EXPORT_URL, post=post_dict)
 
@@ -212,5 +266,9 @@ def _sendPostDict(post_dict):
 
 
 def exportEPublication(epub):
+    """
+    Send `epub` EPublication object to Aleph, where it will be processed by
+    librarians.
+    """
     post_dict = PostData(epub).get_POST_data()
     # sendPostDict(post_dict)  # TODO: uncoment, when test settings at the webform will be implemented
