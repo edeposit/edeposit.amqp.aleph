aceb6e4323401954fe52f634f398a4724a3ee162
Bystroushaak <bystrousak@kitakitsune.org>
2014-03-10 15:32:07 +0100

    Documentation of aleph.datastructures.requests ported to napoleon google style.

--- src/edeposit/amqp/aleph/datastructures/requests.py
+++ src/edeposit/amqp/aleph/datastructures/requests.py
@@ -4,31 +4,44 @@
 # Interpreter version: python 2.7
 #
 #= Imports ====================================================================
+"""
+Request structures, on which :func:`aleph.reactToAMQPMessage` reacts.
+
+All strucutures defined here are simple dataholders, based on `namedtuple`.
+"""
 from collections import namedtuple
 
 
 class CountRequest(namedtuple("CountRequest", ["query"])):
     """
-    Put one of the Queries to .query property and it will return just
+    Put one of the Queries to .query property and result will be just the
     number of records, instead of records itself.
 
-    This help to saves some of Aleph resources (yeah, it is restricted to give
+    This helps to save some of Aleph resources (yeah, it is restricted to give
     too much queries by license).
 
-    query -- GenericQuery, ISBNQuery, .. *Query structures in this module
+    Attributes:
+        query (Query object): GenericQuery, ISBNQuery, .. (Any)Query structure
+              defined in :class:`aleph` module.
 
-    reactToAMQPMessage() returns CountResult as response.
+    See Also:
+        :func:`aleph.reactToAMQPMessage` returns
+        :class:`aleph.datastructures.results.CountResult` as response.
     """
     pass
 
 
 class SearchRequest(namedtuple("SearchRequest", ['query'])):
     """
-    Perform search in Aleph with given query.
+    Perform search in Aleph with given `query`.
 
-    query -- GenericQuery, ISBNQuery, .. *Query structures in this module
+    Attributes:
+        query (Query object): GenericQuery, ISBNQuery, .. (Any)Query structure
+              defined in :class:`aleph` module.
 
-    reactToAMQPMessage() returns SearchResult as response.
+    See Also:
+        :func:`aleph.reactToAMQPMessage` returns
+        :class:`aleph.datastructures.results.SearchResult` as response.
     """
     pass
 
@@ -37,7 +50,12 @@ class ISBNValidationRequest(namedtuple("ISBNValidationRequest", ['ISBN'])):
     """
     Validate given ISBN.
 
-    reactToAMQPMessage() returns ISBNValidationResult as response.
+    Attributes:
+        ISBN (str): ISBN, which will be validated.
+
+    See Also:
+        :func:`aleph.reactToAMQPMessage` returns
+        :class:`aleph.datastructures.results.ISBNValidationResult` as response.
     """
     pass
 
@@ -46,11 +64,19 @@ class ExportRequest(namedtuple("AlephExport", ['epublication'])):
     """
     Request to export data to Aleph.
 
-    ISBN, nazev, Místo vydání, Měsíc a rok vydání, Pořadí vydání, Zpracovatel
-    záznamu, vazba/forma, Formát (poze pro epublikace), Nakladatel has to be
-    present, or AssertionError will be thrown. ISBN has to be valid, or
-    request will be rejected with ExportException.
+    Attributes:
+        epublication: :class:`aleph.datastructures.epublication.EPublication`
+                      structure, which will be exported to Aleph
 
-    epublication -- EPublication structure, which will be exported to Aleph
+    Warning:
+        `ISBN`, `nazev`, `Místo vydání`, `Měsíc a rok vydání`, `Pořadí vydání`,
+        `Zpracovatel záznamu`, `vazba/forma`, `Formát (poze pro epublikace)`
+        and `Nakladatel` has to be present, or AssertionError will be thrown.
+
+        ISBN has to be valid, or request will be rejected with ExportException.
+
+    See Also:
+        :func:`aleph.reactToAMQPMessage` returns
+        :class:`aleph.datastructures.results.ExportResult` as response.
     """
     pass
