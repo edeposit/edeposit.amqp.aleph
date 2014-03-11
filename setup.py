28733ef0b2c780db25eb04667a0771079015ce6e
Bystroushaak <bystrousak@kitakitsune.org>
2014-03-11 15:43:20 +0100

    All source files are now documented with google style docstrings.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 
-version = '1.2.0'
+version = '1.2.1'
 long_description = "\n\n".join([
     open('README.rst').read(),
     open('CONTRIBUTORS.txt').read(),
