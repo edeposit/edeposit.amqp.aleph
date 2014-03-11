1e9a4da81723bd202746ec706684ed92adc8c472
Bystroushaak <bystrousak@kitakitsune.org>
2014-03-11 10:48:38 +0100

    Documentation of isbn.py changed to napoleondoc google style.

--- src/edeposit/amqp/aleph/isbn.py
+++ src/edeposit/amqp/aleph/isbn.py
@@ -5,8 +5,10 @@
 #
 """
 This module is providing funcionality to validate ISBN checksums and also
-allows to compute ISBN checksum digits.
+allows to compute ISBN's checksum digits.a
 """
+
+from functools import wraps
 
 
 #= Functions & objects ========================================================
@@ -14,7 +16,11 @@ def _clean_isbn(isbn):
     """
     Remove all non-digit and non "x" characters from given string.
 
-    Return list of numbers (if "x" is found, it is converted to 10).
+    Args:
+        isbn (str): isbn string, which will be cleaned.
+
+    Returns:
+        list: array of numbers (if "x" is found, it is converted to 10).
     """
     if type(isbn) == str:
         isbn = list(isbn.lower())
@@ -26,66 +32,101 @@ def _clean_isbn(isbn):
     return map(lambda x: 10 if x == "x" else int(x), isbn)
 
 
-def isbn_cleaner(fn):
+def _isbn_cleaner(fn):
     """
-    Decorator for calling other functions from this modules.
+    Decorator for calling other functions from this modules. Purpose of this
+    decorator is to clean the ISBN string from garbage and return list.
+
+    Args:
+        fn (function): function in which will be :func:`_clean_isbn(isbn)` call
+                       wrapped.
     """
+    @wraps(fn)
     def wrapper(isbn):
         return fn(_clean_isbn(isbn))
 
     return wrapper
 
 
-@isbn_cleaner
+@_isbn_cleaner
 def get_isbn10_checksum(isbn):
     """
-    Return checksum for given `isbn`.
+    Args:
+        isbn (str/list): ISBN number as string or list of digits
 
-    Function expects that `isbn` is only 9 digits long.
+    Warning:
+        Function expects that `isbn` is only 9 digits long.
 
-    Returns: int -- last checksum digit
+    Returns:
+        int: last checksum digit for given `isbn`.
     """
     return sum([(i + 1) * x for i, x in enumerate(isbn)]) % 11
 
 
-@isbn_cleaner
+@_isbn_cleaner
 def is_isbn10_valid(isbn):
-    """Check if given `isbn` is valid."""
+    """
+    Check if given `isbn` 10 is valid.
+
+    Args:
+        isbn (str/list): ISBN number as string or list of digits
+
+    Returns:
+        bool: True if ISBN is valid
+    """
     if len(isbn) != 10:
         return False
 
     return get_isbn10_checksum(isbn[:-1]) == isbn[-1]
 
 
-@isbn_cleaner
+@_isbn_cleaner
 def get_isbn13_checksum(isbn):
     """
-    Return `isbn`'s checksum number.
+    Args:
+        isbn (str/list): ISBN number as string or list of digits
 
-    Function expects that `isbn` is only 12 digits long.
+    Warning:
+        Function expects that `isbn` is only 12 digits long.
 
-    Returns: int -- last checksum digit
+    Returns:
+        int: last checksum digit for given `isbn`.
     """
     multipliers = map(lambda x: int(x), list("13" * 6))
     return 10 - sum([i * x for i, x in zip(multipliers, isbn)]) % 10
 
 
-@isbn_cleaner
+@_isbn_cleaner
 def is_isbn13_valid(isbn):
-    """Check if given `isbn` is valid."""
+    """
+    Check if given `isbn` 13 is valid.
+
+    Args:
+        isbn (str/list): ISBN number as string or list of digits
+
+    Returns:
+        bool: True if ISBN is valid
+    """
     if len(isbn) != 13:
         return False
 
     return get_isbn13_checksum(isbn[:-1]) == isbn[-1]
 
 
-@isbn_cleaner
+@_isbn_cleaner
 def is_valid_isbn(isbn):
     """
     Validate given `isbn`.
 
-    Function doesn't require `isbn` type to be specified (it can be both 10/13
-    isbn's versions).
+    Args:
+        isbn (str/list): ISBN number as string or list of digits
+
+    Note:
+        Function doesn't require `isbn` type to be specified (it can be both
+        10/13 isbn's versions).
+
+    Returns:
+        bool: True if ISBN is valid
     """
     length = len(isbn)
 
@@ -95,4 +136,3 @@ def is_valid_isbn(isbn):
         return is_isbn13_valid(isbn)
 
     return False
-
