ebdefcf954423453c54d5557e6fcb204a996bad6
Bystroushaak <bystrousak@kitakitsune.org>
2014-06-25 15:07:00 +0200

    Added docstrings to alephrecord. SemanticInfo is now parsed automatically.
    Bump #26.

--- src/edeposit/amqp/aleph/datastructures/alephrecord.py
+++ src/edeposit/amqp/aleph/datastructures/alephrecord.py
@@ -2,15 +2,36 @@
 # -*- coding: utf-8 -*-
 #
 # Interpreter version: python 2.7
-#
-#= Imports ====================================================================
+"""
+Following structures are used to represent informations returned from Aleph.
+
+API
+---
+"""
+# Imports =====================================================================
 from collections import namedtuple
+
+
+from ..marcxml import MARCXMLRecord
 import convertor
 
 
+# Structures ==================================================================
 class SemanticInfo(namedtuple("SemanticInfo", ["hasAcquisitionFields",
                                                "hasISBNAgencyFields",
                                                "hasCatalogizationFields"])):
+    """
+    This structure is used to represent informations about export progress in
+    Aleph.
+
+    It contains informations about state of the record, so it can be tracked
+    from edeposit project.
+
+    Attributes:
+        hasAcquisitionFields (bool): Was the record aproved by acquisition?
+        hasISBNAgencyFields (bool):  Was the record approved by ISBN agency?
+        hasCatalogizationFields (bool): Does it have catalogization fileds.
+    """
     pass
 
 
@@ -33,20 +54,35 @@ class AlephRecord(namedtuple("AlephRecord", ['base',
                          fetch documents from Aleph if you know this.
         xml (str): MARC XML source returned from Aleph. Quite complicated
                    stuff.
-        epublication (EPublication): Parsed ``.xml`` to :class:`.EPublication`
-                                     structure.
+        epublication (namedtuple, default None): Parsed :attr:`.xml` to
+                     :class:`.EPublication` structure.
+        semantic_info (namedtuple, default None): Export progress informations
+                      from :attr:`.xml` attribute represented as
+                      :class:`SemanticInfo` structure.
+
+    Note:
+        :attr:`semantic_info` and :attr:`epublication` attributes are parsed
+        automatically from :attr:`xml` if not provided by user.
     """
-    def __new__(cls, base, library, docNumber, xml, epublication,
+    def __new__(cls, base, library, docNumber, xml, epublication=None,
                 semantic_info=None):
-        if not semantic_info and xml.strip():
-            semantic_info = convertor.toSemanticInfo(xml)
+        if xml.strip():
+            parsed = xml
+            if not isinstance(parsed, MARCXMLRecord):  # caching
+                parsed = MARCXMLRecord(str(parsed))
+
+            if not semantic_info:
+                semantic_info = convertor.toSemanticInfo(parsed)
+
+            if not epublication:
+                epublication = convertor.toEPublication(parsed)
 
         return super(AlephRecord, cls).__new__(
             cls,
             base,
             library,
             docNumber,
-            xml,
+            str(xml),
             epublication,
             semantic_info
         )
