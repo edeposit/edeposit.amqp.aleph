36049ddbffe12496d96e7a00c82ada52de6a29e3
Bystroushaak <bystrousak@kitakitsune.org>
2014-10-20 15:09:05 +0200

    #36: I've decided to rewrite tests from robot framework to pytest.

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,63 @@
-#! /usr/bin/env sh
+#! /usr/bin/env bash
 
-pybot -W 80 --pythonpath src/edeposit/amqp/aleph/tests/:src src/edeposit/amqp/aleph/tests/
+PYTHONPATH="$PYTHONPATH:src/"
+TEST_PATH="src/edeposit/amqp/aleph/tests"
+
+function show_help {
+    echo -e "Usage: $0 [-h] [-a] [-i] [-u]"
+    echo
+    echo -e "\t-h"
+    echo -e "\t\tShow this help."
+    echo -e "\t-a"
+    echo -e "\t\tRun all tests."
+    echo -e "\t-i"
+    echo -e "\t\tRun integration test (requires sudo)."
+    echo -e "\t-u"
+    echo -e "\t\tRun unittest."
+    echo
+    exit;
+}
+
+function run_all_tests {
+    sudo env PYTHONPATH=$PYTHONPATH py.test $TEST_PATH;
+    exit
+}
+
+function run_int_tests {
+    sudo env PYTHONPATH=$PYTHONPATH py.test "$TEST_PATH/integration";
+    exit
+}
+
+function run_unit_tests {
+    py.test "$TEST_PATH/unit";
+    exit
+}
+
+while getopts "haiu" optname; do
+    case "$optname" in
+        "a")
+            run_all_tests;
+        ;;
+        "i")
+            run_int_tests;
+        ;;
+        "u")
+            run_unit_tests;
+        ;;
+        "h")
+            show_help;
+        ;;
+        "?")
+            echo "Unknown option $OPTARG"
+        ;;
+        ":")
+            echo "No argument value for option $OPTARG"
+        ;;
+        *)
+            # Should not occur
+            echo "Unknown error while processing options"
+        ;;
+    esac
+done
+
+show_help;
