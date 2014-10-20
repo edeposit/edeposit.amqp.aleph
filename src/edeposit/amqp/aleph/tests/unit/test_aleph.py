f43ab856fdebefe4e8e725edca5d97101e5d3fe1
Bystroushaak <bystrousak@kitakitsune.org>
2014-10-20 15:10:52 +0200

    #36: Added unittests of aleph; variables, exceptions and DocumentID.

---
+++ src/edeposit/amqp/aleph/tests/unit/test_aleph.py
@@ -0,0 +1,59 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Interpreter version: python 2.7
+#
+# Imports =====================================================================
+import pytest
+
+from edeposit.amqp.aleph import aleph
+
+
+# Variables ===================================================================
+
+
+
+# Functions & objects =========================================================
+def test_variables():
+    assert "/" in aleph.SEARCH_URL_TEMPLATE
+    assert "/" in aleph.SET_URL_TEMPLATE
+    assert "/" in aleph.DOC_URL_TEMPLATE
+    assert "/" in aleph.OAI_DOC_URL_TEMPLATE
+    assert "/" in aleph.RECORD_URL_TEMPLATE
+
+    int(aleph.MAX_RECORDS)
+
+
+def test_AlephException():
+    with pytest.raises(aleph.AlephException):
+        raise aleph.AlephException("Xe")
+
+
+def test_InvalidAlephBaseException():
+    with pytest.raises(aleph.InvalidAlephBaseException):
+        raise aleph.InvalidAlephBaseException("Xe")
+
+
+def test_InvalidAlephFieldException():
+    with pytest.raises(aleph.InvalidAlephFieldException):
+        raise aleph.InvalidAlephFieldException("Xe")
+
+
+def test_LibraryNotFoundException():
+    with pytest.raises(aleph.LibraryNotFoundException):
+        raise aleph.LibraryNotFoundException("Xe")
+
+
+def test_DocumentNotFoundException():
+    with pytest.raises(aleph.DocumentNotFoundException):
+        raise aleph.DocumentNotFoundException("Xe")
+
+
+def test_DocumentID():
+    did = aleph.DocumentID("someid", "somelibrary", "somebase")
+
+    assert did.id == "someid"
+    assert did.library == "somelibrary"
+    assert did.base == "somebase"
+
+
