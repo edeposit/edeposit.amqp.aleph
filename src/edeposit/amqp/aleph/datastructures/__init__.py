9462a9d918fd7cf10cf5b7959a78ed9436917972
Bystroushaak <bystrousak@kitakitsune.org>
2015-11-20 12:41:36 +0100

    #58: EPeriodical and EPeriodicalSemanticInfo imported to module level.

--- src/edeposit/amqp/aleph/datastructures/__init__.py
+++ src/edeposit/amqp/aleph/datastructures/__init__.py
@@ -1,5 +1,9 @@
-# __import__('pkg_resources').declare_namespace(__name__)
-
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Interpreter version: python 2.7
+#
+# Imports =====================================================================
 from results import *
 from requests import *
 
@@ -8,3 +12,6 @@ from format_enum import FormatEnum
 from alephrecord import AlephRecord
 from epublication import EPublication
 from semanticinfo import SemanticInfo
+
+from eperiodical import EPeriodical
+from eperiodical_semantic_info import EPeriodicalSemanticInfo
