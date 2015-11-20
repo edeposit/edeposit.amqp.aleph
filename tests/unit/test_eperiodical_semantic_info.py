7733ec9e027a86f8199ba55e2233d995e9e09a42
Bystroushaak <bystrousak@kitakitsune.org>
2015-11-20 12:42:48 +0100

    #58: Added few tests of EPeriodicalSemanticInfo.

--- tests/unit/test_eperiodical_semantic_info.py
+++ tests/unit/test_eperiodical_semantic_info.py
@@ -4,17 +4,21 @@
 # Interpreter version: python 2.7
 #
 # Imports =====================================================================
+import pytest
+
+from test_eperiodical import periodical_example
+
+from aleph import EPeriodicalSemanticInfo
 
 
+# Tests =======================================================================
+def test_from_xml(periodical_example):
+    epsi = EPeriodicalSemanticInfo.from_xml(periodical_example)
 
-# Variables ===================================================================
-
-
-
-# Functions & classes =========================================================
-
-
-
-# Main program ================================================================
-if __name__ == '__main__':
-    pass
+    assert epsi.hasAcquisitionFields
+    assert set(epsi.acquisitionFields) == set(['sk20151111', 'pepe'])
+    assert not epsi.isClosed
+    assert epsi.isSummaryRecord
+    assert epsi.contentOfFMT == "SE"
+    assert not epsi.parsedSummaryRecordSysNumber
+    assert not epsi.summaryRecordSysNumber
